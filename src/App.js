5bee61115281b4dfd703248decdc49a25c77d548
unknown <dancesfire@gmail.com>
2021-02-22 14:08:23 -0800

    back end front end, and database connected

--- src/App.js
+++ src/App.js
@@ -15,20 +15,13 @@ import Characters from './components/Rickipedia/Characters'
 import Locations from './components/Rickipedia/Locations'
 import Theories from './components/Rickipedia/Theories'
 import Episodes from './components/Rickipedia/Episodes'
-<<<<<<< HEAD
-import axios from 'axios'
-
-
-
-=======
 // const axios = require('axios')
->>>>>>> 77d1506dcc9ffbdcfb5949737d9cb61c3c8a72de
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const user = localStorage.getItem('jwtToken');
   return <Route {...rest} render={(props) => {
-      return user ? <Component {...rest} {...props} /> : <Redirect to="/login" />
-    }}
+    return user ? <Component {...rest} {...props} /> : <Redirect to="/login" />
+  }}
   />;
 }
 
@@ -39,96 +32,88 @@ function App() {
   const [characters, setCharacters] = useState([])
   const [episodes, setEpisodes] = useState([])
   const [locations, setLocations] = useState([])
-}
-​
-  useEffect(() => {
-<<<<<<< HEAD
-    axios.get('/api/characters')
-=======
-    fetch('http://localhost:8000/wiki/characters')
->>>>>>> 77d1506dcc9ffbdcfb5949737d9cb61c3c8a72de
+
+
+useEffect(() => {
+  fetch('http://localhost:8000/wiki/characters')
     .then(res => res.json())
     .then(characterData => {
       setCharacters(characterData)
     })
-  }, [])
-​
-  useEffect(() => {
-    fetch('http://localhost:8000/wiki/episodes')
+}, [])
+
+useEffect(() => {
+  fetch('http://localhost:8000/wiki/episodes')
     .then(res => res.json())
     .then(episodeData => {
       setEpisodes(episodeData)
     })
-  }, [])
-​
-  useEffect(() => {
-    fetch('http://localhost:8000/wiki/locations')
+}, [])
+
+useEffect(() => {
+  fetch('http://localhost:8000/wiki/locations')
     .then(res => res.json())
     .then(locationData => {
       setLocations(locationData)
     })
-  }, [])
+}, [])
 
-  useEffect(() => {
-    let token;
-    if (!localStorage.getItem('jwtToken')) {
-      setIsAuthenticated(false);
-    } else {
-      token = jwt_decode(localStorage.getItem('jwtToken'));
-      setAuthToken(localStorage.jwtToken);
-      setCurrentUser(token);
-      setIsAuthenticated(true);
-    }
-  }, []);
-
-  const nowCurrentUser = (userData) => {
-    console.log('nowCurrentUser is working...');
-    setCurrentUser(userData);
+useEffect(() => {
+  let token;
+  if (!localStorage.getItem('jwtToken')) {
+    setIsAuthenticated(false);
+  } else {
+    token = jwt_decode(localStorage.getItem('jwtToken'));
+    setAuthToken(localStorage.jwtToken);
+    setCurrentUser(token);
     setIsAuthenticated(true);
-  };
-
-  const handleLogout = () => {
-    if (localStorage.getItem('jwtToken')) {
-      localStorage.removeItem('jwtToken');
-      setCurrentUser(null);
-      setIsAuthenticated(false);
-    }
   }
+}, []);
 
-  console.log('Current User', currentUser);
-  console.log('Authenicated', isAuthenticated);
+const nowCurrentUser = (userData) => {
+  console.log('nowCurrentUser is working...');
+  setCurrentUser(userData);
+  setIsAuthenticated(true);
+};
 
-  return (
-    <div>
-      <Navbar handleLogout={handleLogout} isAuth={isAuthenticated} />
-      <div className="container mt-5">
-        <Switch>
-          <Route path="/signup" component={ Signup } />
-          <Route 
-            path="/login" 
-            render={ (props) => <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser}/>} 
-          />
-          <Route path="/about" component={ About } />
-          <PrivateRoute path="/profile" component={ Profile } user={currentUser} />
-          <Route exact path="/" component={ Welcome } />
-          
-          <Route path="/home" component={ Home } user={currentUser}/>
-<<<<<<< HEAD
-          <Route path="/characters" render={()=> <Characters characters={characters} />}/>
-          <Route path="/locations" component={ Locations } user={currentUser} locations={locations}/>
-=======
-          <Route path="/characters" render={() => <Characters characters={characters} /> }/>
-          <Route path="/locations" render={() => <Locations locations={locations} /> }/>
->>>>>>> 77d1506dcc9ffbdcfb5949737d9cb61c3c8a72de
-          <Route path="/theories" component={ Theories } user={currentUser}/>
-          <Route path="/episodes" render={() => <Episodes episodes={episodes} /> }/>
-          
-        </Switch>
-      </div>
-      <Footer />
-    </div>
-  );
+const handleLogout = () => {
+  if (localStorage.getItem('jwtToken')) {
+    localStorage.removeItem('jwtToken');
+    setCurrentUser(null);
+    setIsAuthenticated(false);
+  }
 }
+
+console.log('Current User', currentUser);
+console.log('Authenicated', isAuthenticated);
+
+return (
+  <div>
+    <Navbar handleLogout={handleLogout} isAuth={isAuthenticated} />
+    <div className="container mt-5">
+      <Switch>
+        <Route path="/signup" component={Signup} />
+        <Route
+          path="/login"
+          render={(props) => <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser} />}
+        />
+        <Route path="/about" component={About} />
+        <PrivateRoute path="/profile" component={Profile} user={currentUser} />
+        <Route exact path="/" component={Welcome} />
+
+        <Route path="/home" component={Home} user={currentUser} />
+        <Route path="/characters" render={() => <Characters characters={characters} />} />
+        <Route path="/locations" render={() => <Locations locations={locations} />} />
+        <Route path="/theories" component={Theories} user={currentUser} />
+        <Route path="/episodes" render={() => <Episodes episodes={episodes} />} />
+
+      </Switch>
+    </div>
+    <Footer />
+  </div>
+)
+}
+
 
 console.log("Hi guys testing")
 
--- src/components/Rickipedia/Characters.js
+++ src/components/Rickipedia/Characters.js
@@ -2,29 +2,11 @@ import React from 'react';
 import axios from "axios"
 
 function Characters(props) {
-<<<<<<< HEAD
-
-    props.getCharacters()
-    
-    return(
-        <div>
-            {/* {props.characters.map((character, i) => {
-                return (
-                    <li>
-                        <h1>{character.name}</h1>
-                        <img src={character.image}></img>
-                        <p>Species: {character.species}</p>
-                        <p>Status: {character.status}</p>
-                        <p>Origin: {character.origin}</p>
-                    </li>
-                )
-            })} */}
-        </div>
-=======
     console.log(props)
     return(
-       <div></div>
->>>>>>> 77d1506dcc9ffbdcfb5949737d9cb61c3c8a72de
+       <div>
+           <h1>soem</h1>
+       </div>
     )
 }
      
