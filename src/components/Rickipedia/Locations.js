add23d68e6fca6c14b8feba231064fedc3151de0
unknown <dancesfire@gmail.com>
2021-02-22 15:12:20 -0800

    mapped locations

--- src/components/Rickipedia/Locations.js
+++ src/components/Rickipedia/Locations.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+
 function Locations(props) {
-    console.log(props)
-    return(
-        <div></div>
+    return (
+        <div>
+            {props.locations.map(location => (
+                <>
+                <ul>
+                    <li><h2>Name  {location.name}</h2></li>
+                    <li><h4>Dimension: {location.dimension}</h4></li>
+                    <li><h4>Type: {location.type}</h4></li>
+                </ul>
+                </>
+            ))}
+        </div>
     )
 }
 
