5fc1e21b6aaeac01603dd12f2eeafa4875e0baf1
unknown <dancesfire@gmail.com>
2021-02-23 15:21:10 -0800

    debugging update and delete paths

--- src/components/Rickipedia/Locations.js
+++ src/components/Rickipedia/Locations.js
@@ -11,6 +11,8 @@ function Locations(props) {
                     <li><h2>{location.name}</h2></li>
                     <li><h4>Dimension: {location.dimension}</h4></li>
                     <li><h4>Type: {location.type}</h4></li>
+                    <hr></hr>
+                    
                 </ul>
                 </div>
             ))}
