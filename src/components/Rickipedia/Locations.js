afbbea7dddad4f786a7e9cdee41faa3f9fcd2db5
unknown <dancesfire@gmail.com>
2021-02-24 14:45:39 -0800

    theory working, working on location buttons

--- src/components/Rickipedia/Locations.js
+++ src/components/Rickipedia/Locations.js
@@ -6,14 +6,17 @@ function Locations(props) {
     return (
         <div class="locationBody" >
             {props.locations.map(location => (
-                <div class="locations">
-                <ul>
-                    <li><h3>{location.name}</h3></li>
-                    <li><h4>Dimension: {location.dimension}</h4></li>
-                    <li><h4>Type: {location.type}</h4></li>
+                <div class="dropdown">
+                    <ul>
+                <div class="divider">
+                <li><button class="dropbtn"><h3>{location.name}</h3></button></li>
+                </div>
+                <div class="dropdown-content">
+                <li> <h4>Dimension: {location.dimension}</h4></li>
+                <li>  <h4>Type: {location.type}</h4></li>
                     
-                    
-                </ul>
+                    </div>
+                    </ul>
                 </div>
             ))}
         </div>
