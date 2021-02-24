38774efd825a8e9683e6ef273e503065e7dada81
unknown <dancesfire@gmail.com>
2021-02-24 14:59:10 -0800

    got hover on locations working

--- src/components/Rickipedia/Locations.js
+++ src/components/Rickipedia/Locations.js
@@ -8,14 +8,14 @@ function Locations(props) {
             {props.locations.map(location => (
                 <div class="dropdown">
                     <ul>
-                <div class="divider">
-                <li><button class="dropbtn"><h3>{location.name}</h3></button></li>
-                </div>
-                <div class="dropdown-content">
-                <li> <h4>Dimension: {location.dimension}</h4></li>
-                <li>  <h4>Type: {location.type}</h4></li>
-                    
-                    </div>
+                        <div class="divider">
+                            <li><h3 class="dropbtn">{location.name}</h3></li>
+                        </div>
+                        <div class="dropdown-content">
+                            <li> <h4>Dimension: {location.dimension}</h4></li>
+                            <li>  <h4>Type: {location.type}</h4></li>
+
+                        </div>
                     </ul>
                 </div>
             ))}
