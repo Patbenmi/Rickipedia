a5f7fa777af5b72761b51f6117fd45802922acf0
Blake DeGraw <bbdegraw@gmail.com>
2021-02-24 13:07:54 -0800

    made minor cosmetic changes to episodes and locations pages

--- src/components/Rickipedia/Episode.js
+++ src/components/Rickipedia/Episode.js
@@ -7,7 +7,7 @@ function Episode(props) {
     <div>
       <a href={props.episodes[props.match.params.id].link} target='_blank'><h1>{props.episodes[props.match.params.id].name}</h1></a>
       <p>{props.episodes[props.match.params.id].episode}</p>
-      <p>{props.episodes[props.match.params.id].date}</p>
+      <p>Aired on: <b>{props.episodes[props.match.params.id].date}</b></p>
       <p>{props.episodes[props.match.params.id].synopsis}</p>
     </div>
   )
