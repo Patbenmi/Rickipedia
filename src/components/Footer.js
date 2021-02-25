421f0fd1dfc0a9099116ba1504e7e55c54b3d47a
Patrick Benjamin Nash <patbenmi@Patricks-MacBook-Pro.local>
2021-02-24 17:09:53 -0800

    Changed the footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+    const And = `&`
     return (
         <footer className="footer bg-dark">
             <div className="container text-center">
-                <span className="text-muted">@2021 General Assembly</span>
+                <span className="text-muted">@2021 Blake, Chassity, Jeremiah {And} Patrick</span>
             </div>
         </footer>
     )
