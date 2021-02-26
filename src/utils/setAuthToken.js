b75dffcf5635a34b9f2188f212f844f467546f76
Patrick Benjamin Nash <patbenmi@Patricks-MacBook-Pro.local>
2021-02-25 17:10:13 -0800

    Removing unnecessary console.logs

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -7,7 +7,6 @@ const setAuthToken = token => {
     if (token) {
         // Apply the token to every request header
         axios.defaults.headers.common['Authorization'] = token;
-        console.log(axios.defaults.headers.common);
     } else {
         delete axios.defaults.headers.common['Authorization'];
     }
